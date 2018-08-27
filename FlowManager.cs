b467d8be4670f9b80477fa8081d470549053ccd3
NickVanheer <nickvanheer@live.be>
2018-08-27 17:06:42 +0200

    Example traversal loop Unity
    Added example manager class of how a flow file could be read and
    traversed in Unity

---
+++ FlowManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFlow;
+
+public class FlowManager : MonoBehaviour {
+
+    static FlowManager instance;
+    public static FlowManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new FlowManager();
+            }
+            return instance;
+        }
+    }
+
+    public FlowParser GameFlow;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+
+            GameFlow = new FlowParser();
+            GameFlow.LoadNodes(@"[YOURFLOW].xml");
+
+            Debug.Log("FlowManager initialized");
+
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        GameFlow.AddCaller(EventQueue.Instance);
+        //GameFlow.AddCaller(SoundManager.Instance);
+        //GameFlow.AddCaller(GameManager.Instance);
+        //(...) more references here
+
+        GameFlow.FireTrigger("GameStart");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //We are currently on a node
+        if (GameFlow.IsActive)
+        {
+            //there's an active root node
+            if (GameFlow.CurrentNodeType() == NodeType.RootNode)
+                GameFlow.GoToNextNode();
+
+            if (GameFlow.CurrentNodeType() == NodeType.MethodNode || GameFlow.CurrentNodeType() == NodeType.ConditionNode)
+            {
+                GameFlow.ExecuteCurrentAction();
+                GameFlow.GoToNextNode();
+            }
+        }
+    }
+}
